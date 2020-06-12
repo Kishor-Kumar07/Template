c845fad5ffb9a84c22ba9a4a7d0b5af28b8d1758
dinesh <dkdinesh6688@gmail.com>
2020-06-12 03:12:42 -0700

    val

--- qp-template-master/src/partc.js
+++ qp-template-master/src/partc.js
@@ -22,6 +22,7 @@ class Partc extends Component {
    
     this.state={
       subload:false,
+      noMatch:false,
       noMatchA:false,
       noMatchB:false,
       noMatchC:false,
@@ -285,6 +286,12 @@ class Partc extends Component {
     var totb=''+(this.props.sb.total)
     var qpc=''+qpcMark
     var totc=''+(this.state.total)
+    var tot=''+this.props.header.marks
+   // console.log(tot)
+   var total=parseInt(this.props.sab.total)+parseInt(this.props.sb.total)+parseInt(this.state.total)
+   //console.log(total)
+   var total_abc=''+total
+  // console.log(typeof(total_abc))
     if(qpa!=tota){ 
      this.state.noMatchA=true
     }
@@ -303,10 +310,33 @@ class Partc extends Component {
      if(qpc==totc){
        this.state.noMatchC=false
      }
-    console.log(this.state.noMatchA)
-    console.log(this.state.noMatchB)
-    console.log(this.state.noMatchC)
-    this.setState({ open: true });
+     if(tot!=total_abc){ 
+      this.state.noMatch=true
+      console.log(this.state.noMatch)
+     }
+     if(tot==total_abc){
+       this.state.noMatch=false
+       console.log(this.state.noMatch)
+     }
+    //  if(this.state.noMatchA){
+    //   alert("Total is not equal in Part-A")
+    //  }
+    //  else if(this.state.noMatchB){
+    //   alert("Total is not equal in Part-B")
+    //  }
+    //  else if(this.state.noMatchC){
+    //   alert("Total is not equal in Part-C")
+    //  }
+    //  if(this.props.sab.touched==false){
+    //    alert("Some field is not ")
+    //  }
+    //  if(this.state.noMatchA||this.state.noMatchB||this.state.noMatchC){
+    //    this.setState({open:false})
+    //  }
+    //  else{
+    //    this.setState({open:true})
+    //  }
+    this.setState({open:true})
 }
 
 handleBlur=(field,id)=>(evt)=>{
